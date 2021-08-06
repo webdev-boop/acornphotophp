28049f73dece23f6b141006d0ad5543e25db3955
webdev-boop <84441222+webdev-boop@users.noreply.github.com>
2021-08-06 02:42:53 -0700

    Update reviews.php

--- reviews.php
+++ reviews.php
@@ -7,7 +7,7 @@
 ?>
 <html lang="en">
 <head>
-    <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.5.2/css/bootstrap.min.css">
+   
     <link rel="stylesheet" type="text/css" href="css/bootstrap.css" />
     <meta charset="UTF-8" name="viewport" content="width=device-width, initial-scale=1" />
     <link rel="stylesheet" href="css/styles.css" />
